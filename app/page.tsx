ac28fdb620e5e7808427383f23104a27c2128089
KyleWhitehead <kylewhitehead0033@yahoo.com>
2026-07-26 15:53:05 -0600

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <main style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+      <h1>Text Cleaner</h1>
+      <p>Your app is running correctly.</p>
+    </main>
+  );
+}
